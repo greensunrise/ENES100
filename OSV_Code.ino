c2a7bdc93a44e40f329d9afc3cd1c71d928a35cd
greensunrise <noahastronomer@gmail.com>
2016-11-15 21:06:25 -0500

    Add files via upload

---
+++ OSV_Code.ino
@@ -0,0 +1,89 @@
+/* OSV Code- Rev. 1
+ *  Noah Eckman, Oct 27 2016
+ */
+
+#define marker_number 104
+//Define Pin Numbers
+#define L_PWM 6
+
+#define RIGHT_FORWARD 7 
+#define RIGHT_BACKWARD 4 //Forward
+
+#define R_PWM 10
+
+#define LEFT_BACKWARD 8
+#define LEFT_FORWARD 9 
+
+#define ELEV_PWM 3
+#define ELEV_S 0
+#define ELEV_R 1
+
+#define SYR_PWM 5
+#define SYR_S 2
+
+#define trigPin 11
+#define echoPin 12
+
+//Include Libraries
+#include "enes100.h"
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(18, 19); 
+enes100::RfClient<SoftwareSerial> rf(&mySerial); 
+enes100::Marker marker;
+
+
+void setup() {
+  mySerial.begin(9600); 
+  pinMode(18, INPUT); 
+  pinMode(19, OUTPUT);
+  pinMode(LEFT_FORWARD, OUTPUT); pinMode(LEFT_BACKWARD, OUTPUT); pinMode(L_PWM, OUTPUT);
+  pinMode(RIGHT_FORWARD, OUTPUT); pinMode(RIGHT_BACKWARD, OUTPUT); pinMode(R_PWM, OUTPUT);
+  rf.resetServer();
+ 
+  rf.sendMessage("\nInitiated RF Contact \n"); //sent to mission control
+  get_pos(marker_number);
+  rf.sendMessage("marker_x, marker_y, marker_theta are..."); line(); 
+  rf.sendMessage(marker.x); line(); rf.sendMessage(marker.y); line(); rf.sendMessage(marker.theta); line();
+
+
+
+  if(marker.y > .4) {
+    rf.sendMessage("Greater than y = .4, orienting to -pi/2...");
+    orient((-PI/2)); line(); 
+    rf.sendMessage("Going to y = .4");
+    goto_y(.4, (-PI/2));
+  }
+  else {
+    orient((PI/2));
+    goto_y(.4, (PI/2)); 
+    orient((-PI/2));
+  }
+  
+  if(is_board_present()) {
+    rf.sendMessage("Board is present");
+    orient((PI/2)); 
+    goto_y(1.7, (PI/2));
+    orient(0);
+    goto_x(1.2, 0);
+    orient(-PI/2);
+    goto_y(.4, (-PI/2));
+    orient(0);
+  }
+rf.sendMessage("Board is absent");
+goto_x(2.9, 0);
+
+//lower elevator
+float depth = water_depth();
+line();
+rf.sendMessage(depth);
+salinity();
+//collect water
+}
+
+
+
+
+void loop() {
+  go_forward();
+}
+
--- sketch_oct27b.ino
+++ sketch_oct27b.ino
@@ -1,81 +1,151 @@
+
+
 /* This file is a location for supplemental code, i.e. functions that
  *  have to be called a lot in OSV_Code
  */
 
-
-
-
-void get_pos(int mrknum)
+void get_pos(int mrknum) 
 {
-  while((!rf.receiveMarker(&marker, mrknum))) {
+  while (!rf.receiveMarker(&marker, mrknum)) { 
+    rf.sendMessage("\nThe RF failed to get the position.\n");
+    delay(250);
   }
+ rf.sendMessage("\nGot my position.\n"); 
 }
+
 void line() {
   rf.sendMessage("\n");
 }
 
+void turn_motors_off() {
+  digitalWrite(LEFT_FORWARD, LOW); 
+  digitalWrite(LEFT_BACKWARD, LOW);
+  analogWrite(L_PWM, 0);
+    
+  digitalWrite(RIGHT_FORWARD, LOW); 
+  digitalWrite(RIGHT_BACKWARD, LOW);
+  analogWrite(R_PWM, 0);
+  delay(300);
+}
+
 void orient(float theta_req) {
-  pinMode(L_S, OUTPUT); pinMode(L_R, OUTPUT); pinMode(L_PWM, OUTPUT);
-  pinMode(R_S, OUTPUT); pinMode(R_R, OUTPUT); pinMode(R_PWM, OUTPUT);
   get_pos(marker_number); //Gets its position 
   float d_theta = theta_req - marker.theta;
   int dir;
-  if (d_theta < 0)
+  if (d_theta < -.15)
   { //declare dir as -1 for case switch
     dir = -1;
   }
-  else 
+  else if(d_theta > .15)
   { //declare dir as 1 for case switch
     dir = 1;  
+  }
+  else 
+  {
+    dir = 0;
   }
 
   switch (dir) {
     case -1:
-    line(); rf.sendMessage("Turning Clockwise...");
+    line(); rf.sendMessage("Turning Clockwise, d_theta is"); rf.sendMessage(d_theta);
     while(d_theta < -.15)
     {
       get_pos(marker_number);
       d_theta = theta_req - marker.theta;
-      turn_right();
+      line(); rf.sendMessage(d_theta);
+      turn_left();
+      go_forward();
+      if(d_theta > .15) {
+        continue;
+      }
+      get_pos(marker_number);
+      turn_left();
+      go_backwards();
     }
       break;
       
     case 1:
-      line(); rf.sendMessage("Turning anti-clockwise...");
+      line(); rf.sendMessage("Turning anti-clockwise, d_theta is"); rf.sendMessage(d_theta);
       while(d_theta > .15)
     {
       get_pos(marker_number);
       d_theta = theta_req - marker.theta;
-      turn_left();
+      line(); rf.sendMessage(d_theta);
+      turn_right();
+      go_forward();
+      if(d_theta > .15) {
+        continue;
+      }
+      turn_right();
+      go_backwards();
      } 
+      break;
+    case 0:
       break;
   }
 } 
 
 void turn_left() {
-  digitalWrite(L_S, HIGH); //Is this right? lol who knows
-  digitalWrite(L_R, LOW);
-  analogWrite(L_PWM, HIGH);
+  digitalWrite(LEFT_FORWARD, HIGH); 
+  digitalWrite(LEFT_BACKWARD, LOW);
+  analogWrite(L_PWM, 200);
+
+  digitalWrite(RIGHT_BACKWARD, HIGH); 
+  digitalWrite(RIGHT_FORWARD, LOW);
+  analogWrite(R_PWM, 200);
+  
   delay(100);
-  digitalWrite(L_S, LOW);
-  digitalWrite(L_R, LOW);
-  analogWrite(L_PWM, LOW);
-  delay(1500);
+  
+  turn_motors_off();
+  
+  delay(10);
 }
 void turn_right() {
-  digitalWrite(L_R, HIGH); //Is this right? lol who knows
-  digitalWrite(L_S, LOW);
-  analogWrite(L_PWM, HIGH);
+  digitalWrite(LEFT_FORWARD, LOW); 
+  digitalWrite(LEFT_BACKWARD, HIGH);
+  analogWrite(L_PWM, 200);
+
+  digitalWrite(RIGHT_FORWARD, HIGH); 
+  digitalWrite(RIGHT_BACKWARD, LOW);
+  analogWrite(R_PWM, 200);
+  
   delay(100);
-  digitalWrite(L_R, LOW);
-  digitalWrite(L_S, LOW);
-  analogWrite(L_PWM, LOW);
-  delay(1500);
+  
+  turn_motors_off();
+  
+  delay(10);
+}
+void go_forward() {
+  digitalWrite(LEFT_FORWARD, HIGH); 
+  digitalWrite(LEFT_BACKWARD, LOW);
+  analogWrite(L_PWM, 150);
+
+  digitalWrite(RIGHT_FORWARD, HIGH); 
+  digitalWrite(RIGHT_BACKWARD, LOW);
+  analogWrite(R_PWM, 150);
+  
+  delay(250);
+  
+  turn_motors_off();
+  
+  delay(10);
+}
+void go_backwards() {
+  digitalWrite(LEFT_FORWARD, LOW); 
+  digitalWrite(LEFT_BACKWARD, HIGH);
+  analogWrite(L_PWM, 255);
+
+  digitalWrite(RIGHT_FORWARD, LOW); 
+  digitalWrite(RIGHT_BACKWARD, HIGH);
+  analogWrite(R_PWM, 255);
+  
+  delay(250);
+  turn_motors_off();
+  delay(10);
 }
 
-int is_board_present() { //Returns 0 if board is absent, 1 if board is present.
-  #define trigPin 11
-  #define echoPin 12
+bool is_board_present() { //Returns false if board is absent, true if board is present.
+
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   float rolling_val = 0;
@@ -98,13 +168,101 @@ while (a <= 10) { //Taking avg of 10 measurements
   sum = sum + distance;
   delay(500);
   a += 1;
-
+}
 rolling_val = sum/10;
 if (rolling_val >= 130 || rolling_val <= 0){
-    return(0); //It's not there.
+    return(false); //It's not there.
   }
   else {
-    return(1); //It's there.
+    return(true); //It's there.
   }
+}
+
+
+void goto_y(float y, float theta) {
+   get_pos(marker_number);
+   float d_y = (y - marker.y);
+   rf.sendMessage(d_y);
+  while(abs(d_y) > .1) {
+    rf.sendMessage(d_y); line();
+    go_forward();
+    orient(theta);
+    get_pos(marker_number);
+    d_y = (y - marker.y);
+  } 
+}
+
+void goto_x(float x, float theta) {
+   get_pos(marker_number);
+   orient(theta);
+   float d_x = (x - marker.x);
+  while(abs(d_x) > .1) {
+    rf.sendMessage(d_x); line();
+    go_forward();
+    orient(theta);
+    get_pos(marker_number);
+    d_x = (x - marker.x);
+  } 
+}
+
+void salinity() {
+
+
+int i = 0;
+int sum = 0;
+while(i < 10) {
+  sum += analogRead(A0); 
+}
+float salinity = sum/10;
+
+if (salinity>740)
+{
+  line();
+  rf.sendMessage("Saltwater"); 
+  delay(100);
+  
+}
+if (salinity>650 && salinity<740)
+{
+  line();
+  rf.sendMessage("Freshwater");
+  delay(100);
+}
+else
+{
+  line();
+  rf.sendMessage("No water present. BZZZRP!");
+}
+}
+
+float water_depth() {
+int echoPin = 16;
+int trigPin = 17;                          
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  Serial.begin(9600);
+  double duration, distance; 
+  float distanceReadings[50];
+  float distanceFromWater;
+  int index;
+  float depth;    
+  float sum = 0;                              
+  for(index = 0; index < 50; index++)
+    {
+       digitalWrite(trigPin, LOW);
+       delayMicroseconds(2);
+       digitalWrite(trigPin, HIGH);
+       delayMicroseconds(10);
+       digitalWrite(trigPin, LOW); 
+       duration = pulseIn(echoPin, HIGH);
+       distance = duration/ 2.9/ 2;
+       distanceReadings[index] = distance;
+       Serial.println(distanceReadings[index]);
+       sum = sum + distanceReadings[index];
+       delay(50); 
+    }
+    distanceFromWater = (sum / 50); 
+    depth = 76.5 - distanceFromWater; 
+    return(depth);   
 }
 
